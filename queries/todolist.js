db33b7e137203f9e7a124ffc2d5a0eef8122d167
Angie Munoz <angiepmunoz@gmail.com>
2023-12-18 20:00:53 -0500

    created basic crud queries

--- queries/todolist.js
+++ queries/todolist.js
@@ -10,6 +10,48 @@ const getAllItems = async ()=> {
     }
 }
 
+const getOneItem = async (id) => {
+    try {
+        const oneItem = await db.one('SELECT * FROM todoItems WHERE id=$1', id );
+        return oneItem;
+    } catch (error) {
+        return error
+    }
+}
+
+const createItem = async (item) => {
+    const {name, notes, completed, due_date, time_of_day, date_of_completion, weekly} = item
+    try {
+        const createdItem = await db.one('INSERT INTO todoItems (name, notes, completed, due_date, time_of_day, date_of_completion, weekly) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *', [name, notes, completed, due_date, time_of_day, date_of_completion, weekly]);
+        return createdItem;
+    } catch (error) {
+        return error;
+    }
+}
+
+const updateItem = async (item) => {
+    const {name, notes, completed, due_date, time_of_day, date_of_completion, weekly ,id} = item
+    try {
+        const updateItem = await db.one('UPDATE todoItems SET name=$1, notes=$2, completed=$3, due_date=$4, time_of_day=$5, date_of_completion=$6, weekly=$7 WHERE id=$8 RETURNING *', [name, notes, completed, due_date, time_of_day, date_of_completion, weekly,id])
+        return updateItem
+    } catch (error) {
+        return error;
+    }
+}
+
+const deleteItem = async (id) => {
+    try {
+        const deletedItem = await db.one('DELETE FROM todoItems WHERE id=$1 RETURNING *', id);
+        return deletedItem;
+    } catch (error) {
+        return error;
+    }
+}
+
 module.exports = {
-    getAllItems
+    getAllItems,
+    getOneItem,
+    updateItem,
+    createItem,
+    deleteItem
 }
